2193dac4f434dd5d68d80f2e4ea52413c53105f3
gsc_prukol <gsc.prukol@gmail.com>
2018-12-11 23:22:47 +0200

    виніс код в окремі файли

---
+++ scripts/rand5.php
@@ -0,0 +1,12 @@
+<?php
+    require './scripts/connect.php';
+    $sql = "SELECT * FROM rand5";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<li><a href=".$row[url].">".$row["name"]."</a></li>\n";
+        }
+    }
+    $conn->close();
+?>
---
+++ scripts/top5.php
@@ -0,0 +1,12 @@
+<?php
+    require './scripts/connect.php';
+    $sql = "SELECT * FROM top5";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<li><a href=".$row[url].">".$row["name"]."</a> (".$row["count"].")</li>\n";
+        }
+    }
+    $conn->close();
+?>
