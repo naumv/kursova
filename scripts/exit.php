bef6c48d1f6898dfedb1fe15eb73787da5654723
gsc_prukol <gsc.prukol@gmail.com>
2018-12-12 19:48:49 +0200

    Поправив вихід.

--- scripts/exit.php
+++ scripts/exit.php
@@ -6,9 +6,8 @@
             exit ("Доступ на эту    страницу разрешен только зарегистрированным пользователям. Если вы    зарегистрированы, то войдите на сайт под своим логином и паролем<br><a    href='index.php'>Главная    страница</a>");
         }
           
-    unset($_SESSION['password']);
             unset($_SESSION['login']); 
             unset($_SESSION['idUser']);//    уничтожаем переменные в сессиях
         exit("<html><head><meta    http-equiv='Refresh' content='0;    URL=../index.php'></head></html>");
             // отправляем пользователя на главную страницу.
-            ?>
+?>
