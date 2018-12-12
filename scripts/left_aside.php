a0807d45f2bc802897119616a1dc7fb856470368
gsc_prukol <gsc.prukol@gmail.com>
2018-12-12 21:00:18 +0200

    Помістив вхід в лівий сайдбар.

---
+++ scripts/left_aside.php
@@ -0,0 +1,20 @@
+<?php
+echo <<<HERE
+<form action=".scripts/test_reg.php" method="post">
+    <p>
+        <label>Ваш логін:<br></label>
+        <input name="login" type="text" size="15" maxlength="15">
+    </p>
+    <p>
+        <label>Ваш пароль:<br></label>
+        <input name="password" type="password" size="15" maxlength="15">
+    </p>
+    <p>
+    <input type="submit" name="submit" value="Войти">
+    </p>
+</form>
+    <p>
+    <a href="reg.php">Зареєструватись</a>
+    <p> 
+HERE;
+?>
