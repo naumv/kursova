4bb4cab82b297c1fda46281e77f18a5e97a22962
gsc_prukol <gsc.prukol@gmail.com>
2018-12-14 11:02:45 +0200

    1 перенаправлення

---
+++ scripts/create_group.php
@@ -0,0 +1,5 @@
+<?php
+    session_start();
+    $_SESSION['output'] = "out_create_group.php";
+    exit("<html><head><meta    http-equiv='Refresh' content='0;    URL=../index.php'></head></html>");
+?>
