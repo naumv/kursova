50fe8c3e3236161a23ce0ab753d5ef4287110176
gsc_prukol <gsc.prukol@gmail.com>
2018-12-14 19:14:49 +0200

    Видалений користувач не зможе ввійти.

--- scripts/test_reg.php
+++ scripts/test_reg.php
@@ -1,20 +1,19 @@
 <?php
-    session_start();//  вся процедура работает на сессиях. Именно в ней хранятся данные  пользователя, пока он находится на сайте. Очень важно запустить их в  самом начале странички!!!
+    session_start();
 if (isset($_POST['login'])) 
 { $login = $_POST['login']; 
     if ($login == '') { unset($login);} 
-} //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
+} 
     if (isset($_POST['password'])) { 
         $password=$_POST['password']; 
     if ($password =='') { unset($password);} 
     }
-    //заносим введенный пользователем пароль в переменную $password, если он пустой, то уничтожаем переменную
-if (empty($login) or empty($password)) //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
+  
+if (empty($login) or empty($password)) 
     {
         $_SESSION['error'] = "Будь ласка, заповніть всі поля";
         exit("<html><head><meta    http-equiv='Refresh' content='0;    URL=/index.php'></head></html>");
     }
-    //если логин и пароль введены,то обрабатываем их, чтобы теги и скрипты не работали, мало ли что люди могут ввести
     $login = stripslashes($login);
     $login = htmlspecialchars($login);
 $password = stripslashes($password);
@@ -24,10 +23,10 @@ $password = stripslashes($password);
     $password = trim($password);
     $password = sha1($password);
 // подключаемся к базе
-    include ("connect.php");// файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто измените путь 
-$result = $conn->query("SELECT * FROM users WHERE login='$login'"); //извлекаем из базы все данные о пользователе с введенным логином
-$row = $result->fetch_assoc();
-    if (empty($row['password_hash']))
+    require ("connect.php");
+    $result = $conn->query("SELECT * FROM users WHERE login='$login'"); //извлекаем из базы все данные о пользователе с введенным логином
+    $row = $result->fetch_assoc();
+    if (empty($row['password_hash']) or $row['status'] == 'd')
     {
     //если пользователя с введенным логином не существует
     $_SESSION['error'] = "Користувача {$login} не знайдено";
@@ -44,12 +43,10 @@ $row = $result->fetch_assoc();
     //эти данные очень часто используются, вот их и будет "носить с собой" вошедший пользователь
     unset($_SESSION['error']);
     exit("<html><head><meta    http-equiv='Refresh' content='0;    URL=../index.php'></head></html>");
-    
     }
  else {
     //если пароли не сошлись
-
-    $_SESSION['error'] = "Введено невірний пароль";
+    $_SESSION['error'] = "Введено невірний логін або пароль";
         exit("<html><head><meta    http-equiv='Refresh' content='0;    URL=../index.php'></head></html>");
     }
     }
