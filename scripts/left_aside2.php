49ff4c27180ba47f83dbe0b84da6c78165e23690
gsc_prukol <gsc.prukol@gmail.com>
2018-12-14 11:36:17 +0200

    left_aside_2

--- scripts/left_aside2.php
+++ scripts/left_aside2.php
@@ -1,12 +1,12 @@
 <?php
 echo <<<HERE
-    <form autocomplete="on" class="log_out" action="/scripts/exit.php">
+    <form autocomplete="on" class="log_out" action="./scripts/video50.php">
         <input type="submit" value="Переглянути відео, додане на сайт">
     </form>
-    <form autocomplete="on" class="log_out" action="/scripts/exit.php">
+    <form autocomplete="on" class="log_out" action="./scripts/groups.php">
         <input type="submit" value="Переглянути групи відео">
     </form>
-    <form autocomplete="on" class="log_out" action="/scripts/exit.php">
+    <form autocomplete="on" class="log_out" action="./scripts/rand50.php">
         <input type="submit" value="50 випадкових відео">
     </form>
 HERE;
---
+++ scripts/video50.php
@@ -0,0 +1,5 @@
+<?php
+    session_start();
+    $_SESSION['output'] = "out_video50.php";
+    exit("<html><head><meta    http-equiv='Refresh' content='0;    URL=../index.php'></head></html>");
+?>
