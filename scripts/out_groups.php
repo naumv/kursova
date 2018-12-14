78328c4b39d95e784f97652960d709b33ecad7e1
gsc_prukol <gsc.prukol@gmail.com>
2018-12-14 19:56:19 +0200

    Тепер і при простому виводі це працює.

--- scripts/out_groups.php
+++ scripts/out_groups.php
@@ -6,7 +6,13 @@ require 'connect.php';
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
-                echo "<li>".$row["name"]."</li>\n";
+                echo <<<HERE
+                <li>
+                    <form action="./scripts/video_in_group.php" method="post">
+                        <button name="id_group" type="submit" value="{$row['id_group']}">{$row['name']}</button>
+                </form>
+                </li> 
+HERE;
             }
         }
         $conn->close();
