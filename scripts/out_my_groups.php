9527a6bf55379608fc93a2e921a07c89648e19ac
gsc_prukol <gsc.prukol@gmail.com>
2018-12-14 15:14:41 +0200

    Добавив вивід груп. Тепер треба створити ще одну табличку.

---
+++ scripts/out_my_groups.php
@@ -0,0 +1,13 @@
+<?php
+require 'connect.php';
+        echo "<h2>Ваш список груп</h2>";
+$sql = "SELECT g.name as name  FROM groups g join user_has_group u_h_g using(id_user) where u_h_g.id_user = {$_SESSION['id_user']}";
+$result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "<li>".$row["name"]."</li>\n";
+            }
+        }
+        $conn->close();
+?>
