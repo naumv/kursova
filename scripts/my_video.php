d6c60780de0632a5095ccd6a9820191b97edef40
gsc_prukol <gsc.prukol@gmail.com>
2018-12-15 00:33:21 +0200

    prepare my

--- scripts/my_video.php
+++ scripts/my_video.php
@@ -4,12 +4,16 @@ require 'connect.php';
     {
         $sql = "SELECT name, url from videos order by id_video DESC LIMIT 50";
         echo "<h2>Останні додані відео</h2>";
+        $result = $conn->query($sql);
     }
     else{
         echo "<h2>Останні додані Вами відео</h2>";
-        $sql = "SELECT name, url from videos join user_has_video using (id_video) WHERE id_user = {$_SESSION['id_user']} order by id_video DESC LIMIT 50";
+        $sql = "SELECT name, url from videos join user_has_video using (id_video) 
+        WHERE id_user = ? order by id_video DESC LIMIT 50";
+            $result = $conn->prepare($sql);
+            $result->bind_param("i", $_SESSION['id_user']);
+            $result = $result->execute(); 
     }
-        $result = $conn->query($sql);
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
