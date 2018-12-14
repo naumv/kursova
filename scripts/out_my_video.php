1bba96c7322f89042b8699ffff64a8d3f64692c3
gsc_prukol <gsc.prukol@gmail.com>
2018-12-14 15:10:19 +0200

    Вивід відео користувача працює.

---
+++ scripts/out_my_video.php
@@ -0,0 +1,13 @@
+<?php
+require 'connect.php';
+        echo "<h2>Ваш список відео</h2>";
+$sql = "SELECT v.name as name , v.url as url FROM videos v join user_has_video u_h_v using(id_video) where u_h_v.id_user = {$_SESSION['id_user']}";
+$result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "<li><a href=".$row[url].">".$row["name"]."</a></li>\n";
+            }
+        }
+        $conn->close();
+?>
