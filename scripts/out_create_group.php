e393a79dc9ea1f19e985e226fac6e0e71317043b
gsc_prukol <gsc.prukol@gmail.com>
2018-12-15 16:37:50 +0200

    max_len in create

--- scripts/out_create_group.php
+++ scripts/out_create_group.php
@@ -10,7 +10,7 @@ echo <<<HERE
 <form autocomplete="off" class="create_group" action="./scripts/save_group.php" method="post">
     <p>
     <label>Ім'я групи:<br></label>
-    <input name="name" type="text" size="80%" maxlength="80">
+    <input name="name" type="text" size="80%" maxlength="79">
 </p>
     <input type="submit" value="Додати групу">
 </form>
--- scripts/out_create_video.php
+++ scripts/out_create_video.php
@@ -10,11 +10,11 @@ echo <<<HERE
 <form autocomplete="off" class="create_video" action="./scripts/save_video.php" method="post">
     <p>
     <label>Ім'я відео:<br></label>
-    <input name="name" type="text" size="80%" maxlength="200">
+    <input name="name" type="text" size="80%" maxlength="190">
 </p>
 <p>
     <label>Посилання на відео:<br></label>
-    <input name="url" type="url" size="80%" maxlength="200">
+    <input name="url" type="url" size="80%" maxlength="190">
 </p>
         <input type="submit" value="Додати відео">
 </form>
