6d9e19955ed2c4c4a0298a8e35553294a69199fa
gsc_prukol <gsc.prukol@gmail.com>
2018-12-15 11:24:23 +0200

    Обмежив права на адміністрування.

--- scripts/out_create_group.php
+++ scripts/out_create_group.php
@@ -1,4 +1,10 @@
 <?php
+   session_start();
+   if(empty($_SESSION['login']) or empty($_SESSION['id_user']) or ($_SESSION['status'] != 'a' and $_SESSION['status'] != 'm')) 
+   {
+       $_SESSION['msg'] = "У Вас відсутні права на дану дію";
+       exit("<html><head><meta    http-equiv='Refresh' content='0;    URL=../index.php'></head></html>");
+   }
 echo <<<HERE
 <form autocomplete="off" class="create_group" action="./scripts/save_group.php" method="post">
     <p>
