6a3c1ba135da39d1669a80bb2ad80cbe93ea5c6c
gsc_prukol <gsc.prukol@gmail.com>
2018-12-15 11:44:13 +0200

    Вивід кнопки додати в групу лише для модераторів/адмінів.

--- scripts/out_video50.php
+++ scripts/out_video50.php
@@ -4,7 +4,7 @@ require 'connect.php';
 HERE;
         $sql = "SELECT * FROM video50";
         $result = $conn->query($sql);
-        $row2['name'] = "Name";
+        //$row2['name'] = "Name";
         if ($result->num_rows > 0) {
             // output data of each row
             if (($_SESSION['status'] == 'a' or $_SESSION['status'] == 'm') and isset($_SESSION['id_group']) )
@@ -20,12 +20,16 @@ HERE;
                 <form action="./scripts/add_video_me.php" method="post">
                         <button name="id_video" type="submit" value="{$row['id_video']}">Зберегти</button>
                 </form>
+HERE;
+                if(isset($row2['name'])){
+                echo <<<HERE
                 <form action="./scripts/add_video_to_group.php" method="post">
                         <button name="id_video" type="submit" value="{$row['id_video']}">
                         Додати відео в групу {$row2['name']}
                          </button>
                 </form></li>
 HERE;
+                }
                 }
         }
         
